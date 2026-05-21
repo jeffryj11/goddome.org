160526c6d9ec00422e61d2654fcc03c0c7b3a25f
jeffryj11 <62181077+jeffryj11@users.noreply.github.com>
2026-05-21 00:45:40 -0500

    Remove old patch stylesheet imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
 import '@/styles/globals.css'
-import '@/styles/header-scroll.css'
-import '@/styles/hero-approved.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
